9ee8d2abdb04f4d31f9801885dec93e38420029e
eldoradodo <rla1eh2dus3@naver.com>
2024-07-02 16:36:12 +0900

    fix : layout tailwind css추가

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -13,8 +13,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
-    </html>
+    <body>
+      <header className="bg-lime-600 text-white p-4">
+        <h1 className="text-2xl font-bold">포켓몬 도감</h1>
+      </header>
+      <main className="container mx-auto p-4">
+        {children}
+      </main>
+      <footer className="bg-lime-600 text-white p-4 text-center">
+        <p>&copy; 2024 Pokemon. All rights reserved.</p>
+      </footer>
+    </body>
+  </html>
   );
 }
  // children 렌더링. 레이아웃 컴포넌트가 모든 페이지 컴포넌트를 감싸도록 함.
