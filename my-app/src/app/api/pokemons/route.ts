7ff38d8132cef0ae3ee47658081c1b2cf4e1f7a1
eldoradodo <rla1eh2dus3@naver.com>
2024-07-04 16:06:15 +0900

    add : 특정 포켓몬의 세부 정보를 가져오는 API를 설정

--- my-app/src/app/api/pokemons/route.ts
+++ my-app/src/app/api/pokemons/route.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 const TOTAL_POKEMON = 151;
 
 export const GET = async (request: Request) => {
-
+  
   try {
     const allPokemonPromises = Array.from({ length: TOTAL_POKEMON }, (_, index) => {
       const id = index + 1;
@@ -18,7 +18,7 @@ export const GET = async (request: Request) => {
 
     const allPokemonData = allPokemonResponses.map(([response, speciesResponse], index) => {
       const koreanName = speciesResponse.data.names.find(
-          (name: any) => name.language.name === "ko"
+        (name: any) => name.language.name === "ko"
       );
       return { ...response.data, korean_name: koreanName?.name || null };
     });
